774c2ae1b3bfe3ded82d5b032afa311da8031a47
delcode <delcode92@gmail.com>
2023-07-13 15:57:34 +0700

    update

--- ngrok_banget/ngrok_script.sh
+++ ngrok_banget/ngrok_script.sh
@@ -12,10 +12,10 @@ ngrok tcp $SERVER:22 &
 sleep 3
 
 #run curl service > ngrok_json.txt
-curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt 
+curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt & 
 
 
-sleep 6
+sleep 3
 
 
 cd /Users/Admin/Documents/project/raspi_final
