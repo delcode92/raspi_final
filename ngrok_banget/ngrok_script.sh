1f3101aa4c4317572aa2b67f1ee0333ff666d922
delcode <delcode92@gmail.com>
2023-07-13 16:41:39 +0700

    update

--- ngrok_banget/ngrok_script.sh
+++ ngrok_banget/ngrok_script.sh
@@ -10,13 +10,13 @@ cd $PATH_NGROK
 #run ngrok service
 ngrok tcp $SERVER:22 &
 
-sleep 3
+sleep 6
 
 #run curl service > ngrok_json.txt
 curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt & 
 
 
-sleep 3
+sleep 6
 
 
 cd $PATH_PROJ
